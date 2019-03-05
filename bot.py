8824b7a32679f795573ae9a1e276d4f6c66670c0
Ole Knuepfer <48217223+Machtbohne@users.noreply.github.com>
2019-03-05 17:01:56 +0100

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,33 @@
+from discord.ext import commands
+
+client = commands.Bot(command_prefix="/")
+player_dict = dict()
+
+
+@client.event
+async def on_ready():
+    print("Bot ist bereit")
+
+
+@client.command(pass_context=True)
+async def play(ctx, url):
+    channel = ctx.message.author.voice.voice_channel
+    await client.join_voice_channel(channel)
+    server = ctx.message.server
+    voice = client.voice_client_in(server)
+    player = await voice.create_ytdl_player(url)
+    player_dict[server.id] = player
+    await client.send_message(ctx.message.channel, "Spiele `%s` ab" % player.title)
+    player.start()
+
+
+@client.command(pass_context=True)
+async def stop(ctx):
+    server = ctx.message.server
+    player = player_dict[server.id]
+    await client.send_message(ctx.message.channel, "Stoppe `%s` " % player.title)
+    player.stop()
+    del player_dict[server.id]
+
+
+client.run("NTUxNjczMTM2NTM4NDUxOTY5.D2AlXQ.lJw9C6YjR1q8S-2n5iUNfCP4r1Y")
