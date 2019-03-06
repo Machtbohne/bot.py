d9cbd5944b804be4c95d504fd21ef0c7791b7adb
Ole Knuepfer <48217223+Machtbohne@users.noreply.github.com>
2019-03-06 19:04:15 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -29,45 +29,5 @@ async def stop(ctx):
     player.stop()
     del player_dict[server.id]
     
-    
-client.event
-async def on_member_join(member):
    
-    users = json.load(f)
-
-    await update_data(users, member)
-
-    with open('users.json', 'w') as f:
-      json.dump(users, f)
-
-@client.event
-async def on_message(message):
-  with open('users.json', 'r') as f:
-    users = json.load(f)
-
-    await update_data(users, message.author)
-    await add_experience(users, message.author, 5)
-    await level_up(users, message.author, message.channel)
-
-    with open('users.json', 'w') as f:
-      json.dump(users, f)
-
-async def update_data(users, user):
-  if not user.id in users:
-    users[user.id] = {}
-    users[user.id]['experience'] = 0
-    users[user.id]['level'] = 1
-
-async def add_experience(users, user, exp):
-  users[user.id]['experience'] + exp
-
-async def level_up(users, user, channel):
-  experience = users[user.id]['experience']
-  lvl_start = users[user.id]['level']
-  lvl_end = int(experience ** (1/4))
-
-if lvl_start < lvl_end:
-   await client.send_message(ctx.message.channel, "{} Ist Aufgeleveld zu Level {}")
-
-
-client.run(str(os.environ.get('BOT_TOKEN')))
+client.run('NTUxNjczMTM2NTM4NDUxOTY5.D2Gcog.ck27miUQZIIsjh_UqV3kqAPDSUI')
