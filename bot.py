57a25f54a4618a77f06e416e8dad593d7c9c3400
Ole Knuepfer <48217223+Machtbohne@users.noreply.github.com>
2019-03-07 16:44:27 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,53 +13,7 @@ player_dict = dict()
 @client.event
 async def on_ready():
     print("Bot ist bereit")
-    
-@bot.event
-async def on_member_join(member):
-    with open("users.json", "r") as f:
-        users = json.load(f)
-
-        await update_data(users, member)
-
-        with open("users.json", "w") as f:
-            json.dump(users, f)
-
-@bot.event
-async def on_message(message):
-    with open("users.json", "r") as f:
-        users = json.load(f)
-
-        if message.author.bot:
-            return
-        else:
-            await update_data(users, message.author)
-            number = random.randint(5,10)
-            await add_experience(users, message.author, number)
-            await level_up(users, message.author, message.channel)
-
-        with open("users.json", "w") as f:
-            json.dump(users, f)
-    await bot.process_commands(message)
-
-async def update_data(users, user):
-    if not user.id in users:
-        users[user.id] = {}
-        users[user.id]["experience"] = 0
-        users[user.id]["level"] = 1
-
-async def add_experience(users, user, exp):
-    users[user.id]["experience"] += exp
-
-async def level_up(users, user, channel):
-    experience = users[user.id]["experience"]
-    lvl_start = users[user.id]["level"]
-    lvl_end = int(experience ** (1/4))
-
-    if lvl_start < lvl_end:
-        await bot.send_message(channel, f":tada: Gratulation {user.mention}, du bist nun Level {lvl_end}!")
-        users[user.id]["level"] = lvl_end
-
-
+   
 @client.command(pass_context=True)
 async def play(ctx, url):
     channel = ctx.message.author.voice.voice_channel
@@ -81,4 +35,4 @@ async def stop(ctx):
     del player_dict[server.id]
     
    
-client.run('NTUxNjczMTM2NTM4NDUxOTY5.D2LKpA.b6sWVT_QQHEPNeXpVdLxfOnngo8')
+client.run("NTUxNjczMTM2NTM4NDUxOTY5.D2LKpA.b6sWVT_QQHEPNeXpVdLxfOnngo8")
